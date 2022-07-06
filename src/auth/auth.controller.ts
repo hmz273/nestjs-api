7c96a2ce9b91c8880ca9a6be7274e7777ff6a1ee
hmz273 <73111873+hmz273@users.noreply.github.com>
2022-07-06 09:16:14 +0100

    auth & jwt config

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,15 +8,11 @@ export class AuthController {
 
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    console.log({
-      dto,
-    });
-
-    return 'login';
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
-  signin() {
-    return 'register';
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
   }
 }
